b29ae0dd1dcc74ba9a3a66c8c7684eb28d435a2e
grenda1999 <grenda.mich1999@gmail.com>
2020-06-21 13:00:59 +0200

    Initial commit

---
+++ src/layouts/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../styles/App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <h1>Witaj świecie!</h1>;
+}
+
+export default App;
